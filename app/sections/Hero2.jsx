b90125c389808ec0b4cd220692149acb5a2f084b
iseyf <c4033962@newcastle.ac.uk>
2026-01-11 17:35:24 +0000

    final fix

--- app/sections/Hero2.jsx
+++ app/sections/Hero2.jsx
@@ -196,7 +196,13 @@ export default function Hero2() {
             <div>
               <button
                 className="flex items-center gap-2 cursor-pointer text-white font-semibold bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:outline-none active:scale-95 transition-transform shadow-lg shadow-gray-500/50 dark:shadow-lg dark:shadow-gray-800/80 rounded-lg text-sm md:px-5 md:py-2.5 px-3 py-2 text-center me-2 mb-2"
-                onClick={() => router.push("https://x.com/cvlabltd")}
+                onClick={() =>
+                  window.open(
+                    "https://www.instagram.com/cvlab.ltd/",
+                    "_blank",
+                    "noopener,noreferrer"
+                  )
+                }
               >
                 <p className="text-lg">Contact us</p>
                 <Image src={xxx} alt="x icon" className="h-8 w-auto" />
@@ -278,8 +284,6 @@ export default function Hero2() {
         .marquee-item {
           flex-shrink: 0;
         }
-
-    
 
         @keyframes marquee {
           0% {
